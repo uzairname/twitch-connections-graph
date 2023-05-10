da28aa1e0cecccd51c791151b56b108ad2191e3c
Uzair <uzair.m6d@gmail.com>
2023-05-10 16:26:17 -0400

    add twitch authorization

--- HttpTriggerPost/__init__.py
+++ HttpTriggerPost/__init__.py
@@ -1,13 +1,45 @@
 import logging
 import os
+import requests
+
 import azure.functions as func
 
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('HttpTriggerPost processed a request.')
+    logging.info('Eventsub endpoint')
 
-    val = os.environ.get('TEST_VAR')
-    logging.info(f"TEST_VAR: {val}")
 
-    return func.HttpResponse(f"Post function response (TEST_VAR: {val})")
+    # get an app access token
+    response = requests.post(
+        "https://id.twitch.tv/oauth2/token",
+        params={
+            "client_id": os.environ["TWITCH_CLIENT_ID"],
+            "client_secret": os.environ["TWITCH_CLIENT_SECRET"],
+            "grant_type": "client_credentials",
+        },
+    )
+
+    if response.status_code != 200:
+        return func.HttpResponse(
+            "Failed to get app access token",
+            status_code=response.status_code,
+        )
+
+    token = response.json()["access_token"]
+
+
+    # Get Twitch EventSub Subscriptions
+    response = requests.get(
+        "https://api.twitch.tv/helix/eventsub/subscriptions",
+        headers={
+            "Authorization": f"Bearer {token}",
+            "Client-Id": os.environ["TWITCH_CLIENT_ID"],
+        },
+    )
+
+    logging.info(f"Got eventsub subscriptions: {response.json()}")
+
+
+    return func.HttpResponse(f"Got eventsub subscriptions: {response.json()}")
 

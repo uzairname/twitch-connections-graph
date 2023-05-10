89e2bd36ae22ec184f14d89a9ed02a306c24c463
Uzair <uzair.m6d@gmail.com>
2023-05-10 14:44:54 -0400

    uncomment

--- HttpTriggerPost/__init__.py
+++ HttpTriggerPost/__init__.py
@@ -6,6 +6,6 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('HttpTriggerPost processed a request.')
 
-    return func.HttpResponse(f"Post function response")
+    return func.HttpResponse(f"Post function response (2)")
 
 

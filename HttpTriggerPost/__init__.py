a79a3dd32a5effdbddcfd8f2f89d9252fa81c9e4
Uzair <uzair.m6d@gmail.com>
2023-05-10 15:55:22 -0400

    print env var

--- HttpTriggerPost/__init__.py
+++ HttpTriggerPost/__init__.py
@@ -1,11 +1,13 @@
 import logging
-
+import os
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('HttpTriggerPost processed a request.')
 
-    return func.HttpResponse(f"Post function response (2)")
+    val = os.environ.get('TEST_VAR')
+    logging.info(f"TEST_VAR: {val}")
 
+    return func.HttpResponse(f"Post function response (TEST_VAR: {val})")
 

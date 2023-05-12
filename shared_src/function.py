194f4bd442806612d2802b47d7922bb2bee88b5e
Uzair <uzair.m6d@gmail.com>
2023-05-11 20:53:46 -0400

    push scope

--- shared_src/function.py
+++ shared_src/function.py
@@ -3,7 +3,7 @@ import azure.functions as func
 import os
 from dotenv import load_dotenv
 import sentry_sdk
-from sentry_sdk import capture_event, capture_exception, add_breadcrumb, capture_message
+from sentry_sdk import capture_event, capture_exception, add_breadcrumb, capture_message, push_scope
 from faunadb.client import FaunaClient
 
 
@@ -13,30 +13,39 @@ fauna_client = FaunaClient(secret=os.environ["FAUNADB_SECRET"])
 
 requestnum = 0
 
+sentry_sdk.init(
+    dsn=os.environ["SENTRY_DSN"],
+    traces_sample_rate=1.0
+)
+
 
 def function(f):
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
-        try:
-            global requestnum
-            requestnum += 1
-            sentry_sdk.init(
-                dsn=os.environ["SENTRY_DSN"],
-                traces_sample_rate=1.0
-            )
-            add_breadcrumb(category="function", message=f"Request #{requestnum}. Executing {f.__name__}")
-            http_response = f(*args, **kwargs)
-            capture_message({
-                "message": f"{f.__name__} executed successfully",
-                "level": "info",
-                "extra": {
-                    "function_name": f.__name__,
-                }
-            })
-            return http_response
-        except Exception as e:
-            capture_exception(e)
-            return func.HttpResponse(f"Exception: {e}")
+        with push_scope() as scope:
+            try:
+                scope.set_tag("function", f.__name__)
+                global requestnum
+                requestnum += 1
+                scope.set_tag("requestnum", requestnum)
+
+                add_breadcrumb(category="function", message=f"Request #{requestnum}. Executing {f.__name__}")
+
+                http_response = f(*args, **kwargs)
+                
+                capture_message({
+                    "message": f"{f.__name__} executed successfully",
+                    "level": "info",
+                    "extra": {
+                        "function_name": f.__name__,
+                    }
+                })
+                return http_response
+            
+            except Exception as e:
+                capture_exception(e)
+                return func.HttpResponse(f"Exception: {e}")
+
         
     return wrapper
 
